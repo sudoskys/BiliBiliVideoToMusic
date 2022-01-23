be27dbf8ce870288ba1f72bfc932039c7fd53681
sudoskys <xiaow3493@gmail.com>
2022-01-23 15:48:16 +0800

    build

---
+++ main.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+"""
+__project__ = 'biliaudiodown'
+__file_name__ = 'main'
+__author__ = 'sudoskys'
+__time__ = '2022/1/23 下午3:07'
+__product_name__ = 'PyCharm'
+__version__ = '1月231507'
+# code is far away from bugs with the god，author here https://github.com/sudoskys
+    ____  _                  
+   / __ \(_)___ _____  ____ _
+  / / / / / __ `/ __ \/ __ `/
+ / /_/ / / /_/ / / / / /_/ / 
+/_____/_/\__,_/_/ /_/\__,_/                    
+"""
